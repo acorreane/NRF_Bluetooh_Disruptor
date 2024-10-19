34e40d3873b91df516e109ce394708651450b131
acorreane <acorreane@gmail.com>
2024-10-19 18:53:40 -0300

    Add files via upload

---
+++ Bluetooth_FHSS_Disruptor_AVR328_8M.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <RF24.h>
+#include <nRF24L01.h>
+
+RF24 radio1(9, 10, 1000000); //CE, CS, CLOCK (higher clocks require SPI bus shielding by RF interference)
+#define RFPOWER RF24_PA_MAX  // using NRF+PA (reduce power on use it with signal boosters)
+
+
+void setup() {
+  pinMode(20, OUTPUT); // led ok (mcu running standalone without xtal on MiniCore at 8MHz)
+  pinMode(21, OUTPUT); // led tx
+  Serial.begin(115200);
+  IOsetup();
+  RFsetup();
+}
+
+void IOsetup() {
+  delay(500); Serial.println("[  SYS  ] init");
+  digitalWrite(20, HIGH);
+}
+
+
+void RFsetup() {
+  delay(500); Serial.println("[ RADIO ] init");
+  if (radio1.begin()) { 
+    radio1.stopListening();
+    radio1.setDataRate(RF24_2MBPS);
+    radio1.setAutoAck(false);
+    radio1.startConstCarrier(RFPOWER, 45);
+  delay(200); Serial.println("[ RADIO ] ok");
+  Serial.end();
+  digitalWrite(21, HIGH);
+    } 
+  else {
+    Serial.println("[ RADIO ] fail");
+  }
+}
+
+void loop() {
+  radio1.setChannel(random(0,80)); //run FHSS carrier from 0 to 80
+}
